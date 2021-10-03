a16c5fccc0a92d6ced8f80d6b4db585c89389ef9
ocohen <ocohen@synamedia.com>
2021-10-03 23:06:16 +0300

    Add project files.

---
+++ TimerWindow/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimerWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       Stopwatch stopwatch;
+       bool isTimerRun = false;
+       Thread timerThread;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+
+        }
+
+
+        void runTimer()
+        {
+            while(isTimerRun)
+            {
+                string timerText = stopwatch.Elapsed.ToString();
+                // 00:00:00.000000 ==> 00:00:00
+                timerText = timerText.Substring(0, 8);
+              // this.timerTextBlock.Text = timerText;
+
+                Action<string> action = setTextInvok;
+               // Dispatcher.BeginInvoke(action, timerText);
+                timerTextBlock.Dispatcher.BeginInvoke(action, timerText);
+                Thread.Sleep(1000);
+            }
+        }
+
+        void setTextInvok(string text) 
+        { 
+            this.timerTextBlock.Text = text;
+          //  Thread.Sleep(1000);
+        } 
+
+        private void startTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isTimerRun)
+            {
+                stopwatch.Restart();
+                isTimerRun = true;
+
+                timerThread = new Thread(runTimer);
+                timerThread.Start();
+               // runTimer();
+            }
+        }
+
+        private void stopTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isTimerRun)
+            {
+                stopwatch.Stop();
+                isTimerRun = false;
+            }
+        }
+    }
+}
---
+++ Wpf_5776/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_5776
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+         //   this.Addbutton.MouseEnter += Addbutton_MouseEnter;
+
+           // MessageBox.Show("dasda");
+        }
+
+        private void myCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+                MouseButton mb = e.ChangedButton;
+                Rectangle r = new Rectangle();
+                r.Width = 100;
+                r.Height = 50;
+
+                switch (mb)
+                {
+                    case MouseButton.Left:
+                        r.Fill = Brushes.Blue;
+                        break;
+                    case MouseButton.Middle:
+                        r.Fill = Brushes.Red;
+                        break;
+                    case MouseButton.Right:
+                        r.Fill = Brushes.Pink;
+                        break;
+
+                    case MouseButton.XButton1:
+                        break;
+                    case MouseButton.XButton2:
+                        break;
+                    default:
+                        break;
+                }
+
+
+                myCanvas.Children.Add(r);
+
+                Point p = Mouse.GetPosition(myCanvas);
+
+                Canvas.SetTop(r, p.Y);
+                Canvas.SetLeft(r, p.X);
+         
+
+
+
+
+        }
+
+       
+
+    }
+}
